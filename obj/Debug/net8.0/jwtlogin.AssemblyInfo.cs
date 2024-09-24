616e1f2519813ccc5822aa5bcba53fc3df9ea64f
Yassine Jlassi <yassinej696@gmail.com>
2024-09-24 23:40:00 +0200

    updateArView

--- obj/Debug/net8.0/jwtlogin.AssemblyInfo.cs
+++ obj/Debug/net8.0/jwtlogin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("jwtlogin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d9d6f8ef876a562b978e8fd3117ce9bb76333d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a53869ba500cb4ab4ec45e54e333a879488c3b81")]
 [assembly: System.Reflection.AssemblyProductAttribute("jwtlogin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("jwtlogin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
