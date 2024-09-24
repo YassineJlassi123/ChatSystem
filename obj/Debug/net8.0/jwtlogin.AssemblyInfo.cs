5d9d6f8ef876a562b978e8fd3117ce9bb76333d8
Yassine Jlassi <yassinej696@gmail.com>
2024-09-24 15:48:10 +0200

    testAr

--- obj/Debug/net8.0/jwtlogin.AssemblyInfo.cs
+++ obj/Debug/net8.0/jwtlogin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("jwtlogin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+885d48c14c1066dfa61030982f6811165e2d1772")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47a9d617a1286f3f958fc723d368ae120c773c1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("jwtlogin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("jwtlogin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
